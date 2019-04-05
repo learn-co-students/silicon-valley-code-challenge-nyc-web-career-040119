0b4782d2d27d9544850631a2ae46b73d73835d6d
ferrisboran <ferris@ferryrules.com>
2019-04-05 10:31:00 -0400

    startup added

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,32 @@
-class Startup
+require 'pry'
 
+class Startup
+  attr_reader :founder
+  attr_accessor :name, :domain
+
+  @@all = []
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pivot(domain, name)
+    @domain = domain
+    @name = name
+  end
+
+  def self.find_by_founder(founder)
+    @@all.find { |name| name.founder == founder }
+  end
+
+  def domains
+    @@all.map { |startup| startup.domain }
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,11 +1,16 @@
 require_relative '../config/environment.rb'
 
+
 def reload
   load 'config/environment.rb'
 end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+eatsa = Startup.new("eatsa", "Scott", "Automated Restaurant")
 
+Startup.find_by_founder("Scott")
+binding.pry
+test.name
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
