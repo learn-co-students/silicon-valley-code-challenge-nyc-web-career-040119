96c252348f204d7d550aa7e93162b33a7c26859d
ferrisboran <ferris@ferryrules.com>
2019-04-05 17:55:47 -0400

    @danfyfe @octopiwithhats rocked it

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -26,7 +26,39 @@ class Startup
     @@all.find { |name| name.founder == founder }
   end
 
-  def domains
+  def self.domains
     @@all.map { |startup| startup.domain }
+  end
+
+  def sign_contract(venture_capitalist, type, amount)
+    FundingRound.new(self,venture_capitalist,type,amount)
+  end
+
+  def num_funding_rounds
+    FundingRound.all.select do |funding_round|
+      funding_round.startup == self
+    end.length
+  end
+
+  def total_funds
+    total = 0
+
+    FundingRound.all.select do |funding_round|
+      if funding_round.startup == self
+        total += funding_round.investment
+      end
+    end
+    total
+  end
+
+  def investors
+    FundingRound.all.map { |round| round.venture_capitalist if round.startup == self }.uniq.compact
+  end
+
+  def big_investors
+    VentureCapitalist.tres_commas_club.select do |v_c|
+      self.investors.include?(v_c.name)
+    end
+
   end
 end
