70b23faee1f3450c592c42b188c3dd0282e775a6
OctopiWithHats <sdgranoff@gmail.com>
2019-04-05 10:39:16 -0400

    Starting the rich people function

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,20 @@
 class VentureCapitalist
+  attr_reader :name
+  attr_accessor :total_worth
+
+  @@all = []
+
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+
+    @@all << self
+  end
+
+  def self.tres_commas_club
+    @@all.select do |rich_people|
+      rich_people.total_worth > 1000000000
+    end
+  end
 
 end
