997f8a52726294c78f3cf249841e437ffd42145c
JDM Ubuntu VB <JKDMarks@gmail.com>
2019-04-05 13:28:20 -0400

    Completed Startup, began on VentureCapitalist associations and aggregate methods

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,25 @@
 class VentureCapitalist
+  @@all = []
 
+  attr_reader :name, :total_worth
+
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    self.class.all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    self.all.select do |capitalist|
+      capitalist.total_worth > 1000000000
+    end
+  end
+
+  def offer_contract(startup, type, investment)
+    FundingRound.new(startup, self, type, investment)
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 require_relative '../config/environment.rb'
 
 def reload
@@ -6,6 +8,22 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+
+amazon = Startup.new("Amazon", "Jeff Bezos", "www.amazon.com")
+microsoft = Startup.new("Microsoft", "Bill Gates", "www.microsoft.com")
+facebook = Startup.new("Facebook", "The Zuck", "www.facebook.com")
+
+steve = VentureCapitalist.new("Steve Jobs", 40000000000)
+jeff = VentureCapitalist.new("Jeff Marks", 60000000000)
+goldfinger = VentureCapitalist.new("Goldfinger", 100000000)
+
+microsoft.sign_contract(jeff, "XBox", 1000000)
+facebook.sign_contract(jeff, "Messenger App", 500000)
+microsoft.sign_contract(goldfinger, "motion tracker", 1000000)
+
+jeff.offer_contract(microsoft, "Mind Control", 2500000000)
+
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
