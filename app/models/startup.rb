b8e21c9461bdcc8c8aedd90dbd164c5f411d8c88
Edgar Ong <edgar@minus.com>
2019-04-05 16:16:12 -0400

    almost there

--- app/models/startup.rb
+++ app/models/startup.rb
@@ -1,3 +1,111 @@
-class Startup
+require 'pry'
 
+# - `Startup#name`
+#   - returns a **string** that is the startup's name
+# - `Startup#founder`
+#   - returns a **string** that is the founder's name
+#   - Once a startup is created, the founder cannot be changed.
+# - `Startup#domain`
+#   - returns a **string** that is the startup's domain
+#   - Once a startup is created, the domain cannot be changed.
+# - `Startup#pivot`
+#   - given a string of a **domain** and a string of a **name**, change the domain and name of the startup
+# - `Startup.all`
+#   - should return **all** of the startup instances
+# - `Startup.find_by_founder`
+#   - given a string of a **founder's name**, returns the **first startup** whose founder's name matches
+# - `Startup.domains`
+#   - should return an **array** of all of the different startup domains
+
+class Startup
+  attr_accessor :name
+  attr_reader :founder, :domain
+
+  @@all = []
+
+  def initialize(name, founder, domain)
+    @name = name
+    @founder = founder
+    @domain = domain
+    @@all << self
+  end
+
+  def pivot(domain, name)
+    @name = name
+    @domain = domain
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_founder(founder)
+    @@all.find do |startup|
+      startup.founder == founder
+      # binding.pry
+    end
+  end
+
+  def self.domains
+    @@all.map do |startup|
+      startup.domain
+    end
+  end
+
+
+# Startup#sign_contract
+# given a venture capitalist object, type of investment (as a string), and the amount invested (as a float), creates a new funding round and associates it with that startup and venture capitalist.
+  def sign_contract(venture_capitalist, type, investment, startup)
+    FundingRound.new(venture_capitalist, type, investment, startup)
+  end
+
+# Startup#num_funding_rounds
+# Returns the total number of funding rounds that the startup has gotten
+  def num_funding_rounds
+    FundingRound.all.count do |rounds|
+      rounds.startup == self && rounds.type
+    end
+  end
+
+# Startup#total_funds
+# Returns the total sum of investments that the startup has gotten
+  def total_funds
+    startup_round = FundingRound.all.map do |rounds|
+      rounds.startup == self && rounds.investment
+    end
+
+    startup_funds = startup_round.select do |funds|
+      funds.class == Float
+    end
+
+    total_funds = startup_funds.inject(:+)
+
+  end
+
+# Startup#investors
+# Returns a unique array of all the venture capitalists that have invested in this company
+  def investors
+    FundingRound.all.map do |rounds|
+      if rounds.startup == self
+        rounds.venture_capitalist.name
+      end
+    end.compact
+  end
+
+
+# Startup#big_investors
+# Returns a unique array of all the venture capitalists that have invested in this company and are in the Trés Commas club
+
+  def big_investors
+    #find the vc that is funding the startup
+    #check if the vc is tres_commas_club
+    #return vc name in array if true
+
+    VentureCapitalist.tres_commas_club.map do |vc|
+      investors.select do |investor|
+        investor == vc.name
+      end
+    end.flatten
+  end
+  
 end
--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,40 @@
-class VentureCapitalist
+# - `VentureCapitalist#name`
+#   - returns a **string** that is the venture capitalist's name
+# - `VentureCapitalist#total_worth`
+#   - returns a **number** that is the total worth of this investor (e.g., think of it as how much money they have)
+# - `VentureCapitalist.all`
+#   - returns an array of all venture capitalists
+# - `VentureCapitalist.tres_commas_club`
+#   - returns an array of all venture capitalists in the Trés Commas club (they have more then 1,000,000,000 `total_worth`)
 
+class VentureCapitalist
+  attr_reader :name, :total_worth
+
+  @@all = []
+  def initialize(name, total_worth)
+    @name = name
+    @total_worth = total_worth
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.tres_commas_club
+    @@all.select do |investors|
+      investors.name if investors.total_worth > 1000000000 #billion
+    end
+  end
 end
+
+# - `VentureCapitalist#offer_contract`
+#   - given a **startup object**, type of investment (as a string), and the amount invested (as a float), creates a new funding round and associates it with that startup and venture capitalist.
+# - `VentureCapitalist#funding_rounds`
+#   - returns an array of all funding rounds for that venture capitalist
+# - `VentureCapitalist#portfolio`
+#   - Returns a **unique** list of all startups this venture capitalist has funded
+# - `VentureCapitalist#biggest_investment`
+#   - returns the largest funding round given by this venture capitalist
+# - `VentureCapitalist#invested`
+#   - given a **domain string**, returns the total amount invested in that domain
